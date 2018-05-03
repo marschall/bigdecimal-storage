5bafc3bb32cbc62c802c1d14a310e04689632903
Philippe Marschall <philippe.marschall@gmail.com>
2018-05-03 08:12:03 +0200

    add object size tests

--- bigdecimal-storage/src/test/java/com/github/marschall/bigdecimalstorage/BigDecimalObjectSizeTest.java
+++ bigdecimal-storage/src/test/java/com/github/marschall/bigdecimalstorage/BigDecimalObjectSizeTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.openjdk.jol.info.GraphLayout;
 
 
-class BigDecimal128ObjectSizeTest {
+class BigDecimalObjectSizeTest {
 
   static Stream<BigDecimal> bigDecimals() {
     return Stream.of(
@@ -23,14 +23,17 @@ class BigDecimal128ObjectSizeTest {
   @MethodSource("bigDecimals")
   void size(BigDecimal bigDecimal) {
     BigDecimal128 bigDecimal128 = BigDecimal128.valueOf(bigDecimal);
+    BigDecimal96 bigDecimal96 = BigDecimal96.valueOf(bigDecimal);
 
-    GraphLayout graphLayout123 = GraphLayout.parseInstance(bigDecimal128);
+    GraphLayout graphLayout128 = GraphLayout.parseInstance(bigDecimal128);
+    GraphLayout graphLayout96 = GraphLayout.parseInstance(bigDecimal96);
 
     GraphLayout graphLayout = GraphLayout.parseInstance(bigDecimal);
-    assertThat(graphLayout123.totalSize()).isLessThan(graphLayout.totalSize());
+    assertThat(graphLayout128.totalSize()).isLessThan(graphLayout.totalSize());
+    assertThat(graphLayout96.totalSize()).isLessThan(graphLayout128.totalSize());
 
     graphLayout = GraphLayout.parseInstance(bigDecimal.unscaledValue());
-    assertThat(graphLayout123.totalSize()).isLessThan(graphLayout.totalSize());
+    assertThat(graphLayout128.totalSize()).isLessThan(graphLayout.totalSize());
   }
 
 }
