ea0abe271f0d27fb59a4a301087186993c8046c3
Philippe Marschall <philippe.marschall@gmail.com>
2018-05-06 13:26:36 +0200

    rename output file

--- bigdecimal-storage-benchmark/src/main/bash/run-benchmarks-graal10.sh
+++ bigdecimal-storage-benchmark/src/main/bash/run-benchmarks-graal10.sh
@@ -4,4 +4,4 @@ $JAVA_HOME/bin/java \
  -Xmx32g -Xms32g -Xmn31g \
  --add-opens java.base/java.io=ALL-UNNAMED \
  -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler \
- -jar target/bigdecimal-storage-benchmark-0.2.0-SNAPSHOT.jar graal9.txt
+ -jar target/bigdecimal-storage-benchmark-0.2.0-SNAPSHOT.jar graal10.txt
